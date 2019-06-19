de9883ac09de57d60bc6a68588e3c97043b190be
sbx_user1051 <sbx_user1051@169.254.118.5>
2019-06-19 11:39:09 +0000

    Wed Jun 19 11:39:09 UTC 2019

--- src/test/scala/befaster/solutions/HLO/HelloTest.scala
+++ src/test/scala/befaster/solutions/HLO/HelloTest.scala
@@ -1,5 +1,10 @@
 package befaster.solutions.HLO
 
-class HelloTest {
+import org.scalatest.{FlatSpec, Matchers}
 
+class HelloTest extends FlatSpec with Matchers {
+  it should "return a hello message" in {
+    Hello.hello("stranger") shouldBe "Hello stranger"
+  }
 }
+
