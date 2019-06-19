909cbbbb2eee9ba83298c95d422cc80b88605702
sbx_user1051 <sbx_user1051@169.254.118.5>
2019-06-19 11:49:09 +0000

    Wed Jun 19 11:49:09 UTC 2019

--- src/main/scala/befaster/solutions/HLO/Hello.scala
+++ src/main/scala/befaster/solutions/HLO/Hello.scala
@@ -1,7 +1,7 @@
 package befaster.solutions.HLO
 
 object Hello {
-  def hello(friendName: String): String = "Hello, World!"
+  def helloWorld(friendName: String): String = "Hello, World!"
+  def helloFriend(friendName: String): String = s"Hello, $friendName!"
+  val hello = helloFriend _
 }
-
-
--- src/test/scala/befaster/solutions/HLO/HelloTest.scala
+++ src/test/scala/befaster/solutions/HLO/HelloTest.scala
@@ -3,9 +3,15 @@ package befaster.solutions.HLO
 import org.scalatest.{FlatSpec, Matchers}
 
 class HelloTest extends FlatSpec with Matchers {
-  it should "return a hello message" in {
-    Hello.hello("stranger") shouldBe "Hello, World!"
+  it should "return a hello World message" in {
+    Hello.helloWorld("stranger") shouldBe "Hello, World!"
   }
+  it should "return a hello friend message" in {
+    Hello.helloFriend("stranger") shouldBe "Hello, stranger!"
+  }
+
+  it should "return same as hello friend message" in {
+    Hello.hello("stranger") shouldBe "Hello, stranger!"
+  }
+
 }
-
-
