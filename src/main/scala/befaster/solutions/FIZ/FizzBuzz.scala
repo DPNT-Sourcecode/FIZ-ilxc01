24a264304a41bedd50d05e94bf0e325f143a5d42
sbx_user1051 <sbx_user1051@169.254.118.5>
2019-06-19 12:03:41 +0000

    Wed Jun 19 12:03:41 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -1,7 +1,18 @@
 package befaster.solutions.FIZ
 
-import befaster.runner.SolutionNotImplementedException
-
 object FizzBuzz {
-  def fizzBuzz(number: Int): String = throw new SolutionNotImplementedException()
+  def fizzBuzz(number: Int): String = {
+    val divBy3 = number % 3
+    val divBy5 = number % 5
+
+    if (divBy3 == 0 && divBy5 == 0)
+      "fizz buzz"
+    else if (divBy3 == 0)
+      "fizz"
+    else if (divBy5 == 0)
+      "buzz"
+    else
+      number.toString
+  }
 }
+
--- src/test/scala/befaster/solutions/FIZ/FizzTest.scala
+++ src/test/scala/befaster/solutions/FIZ/FizzTest.scala
@@ -3,6 +3,17 @@ package befaster.solutions.FIZ
 import org.scalatest.{FlatSpec, Matchers}
 
 class FizzTest  extends FlatSpec with Matchers {
+  it should "return fizz" in {
+    FizzBuzz.fizzBuzz(6) shouldBe "fizz"
+  }
 
+  it should "return buzz" in {
+    FizzBuzz.fizzBuzz(10) shouldBe "buzz"
+  }
+
+  it should "return fizz buzz" in {
+    FizzBuzz.fizzBuzz(15) shouldBe "fizz buzz"
+  }
 }
+
 
